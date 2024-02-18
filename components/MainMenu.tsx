698babcbe27aebb4f0227b6bef2c0e01a8e01f36
ralist <danilov@ralist.ru>
2024-02-18 15:57:59 +0400

    add menu

--- components/MainMenu.tsx
+++ components/MainMenu.tsx
@@ -3,14 +3,12 @@ import Link from "next/link";
 
 export default function MainMenu({ user }: { user: AppUser | null }) {
   if (!user) {
-    return (
-      <div>Something went wrong!</div>
-    )
+    return <div>Loading...</div>
   }
 
   return (
     <div className={"mainMenu"}>
-      <p>Welcome, {user.username}</p>
+      <p>Welcome, {user?.username}</p>
       <p>This game is on-chain, that why you have to import or create TON wallet</p>
       <Link href=""> Import wallet </Link>
       <Link href=""> Create wallet </Link>
