317fdf44704d6239a12aa947ca394e5dcffb2c9f
ralist <danilov@ralist.ru>
2024-02-18 15:34:28 +0400

    add db

---
+++ components/MainMenu.tsx
@@ -0,0 +1,19 @@
+import {AppUser} from "@/app/page";
+import Link from "next/link";
+
+export default function MainMenu({ user }: { user: AppUser | null }) {
+  if (!user) {
+    return (
+      <div>Something went wrong!</div>
+    )
+  }
+
+  return (
+    <div className={"mainMenu"}>
+      <p>Welcome, {user.username}</p>
+      <p>This game is on-chain, that why you have to import or create TON wallet</p>
+      <Link href=""> Import wallet </Link>
+      <Link href=""> Create wallet </Link>
+    </div>
+  )
+}
