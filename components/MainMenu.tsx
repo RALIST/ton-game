a85468a8fcdc45c9241ab9a61eaf4aa501eb0561
ralist <danilov@ralist.ru>
2024-02-19 11:40:51 +0400

    design example

--- components/MainMenu.tsx
+++ components/MainMenu.tsx
@@ -1,18 +1,10 @@
 import {AppUser} from "@/app/page";
-import {useEffect, useState} from "react";
 import Image from "next/image";
 
 import footerImage from "../assets/floor.png"
-import headerImage from "../assets/sky.png"
 import bgImage from "../assets/houses.png"
 
 export default function MainMenu({ user }: { user: AppUser | null }) {
-  const [date, setDate] = useState<string | null>(null);
-  useEffect(() => {
-    setDate(new Date().toLocaleDateString())
-  }, []);
-
-
   if (!user) {
     return <div>Loading...</div>
   }
@@ -20,9 +12,6 @@ export default function MainMenu({ user }: { user: AppUser | null }) {
   return (
     <>
       <div className={"screen"}>
-        <div className={"upHeaderImage"}>
-          <Image src={headerImage} alt={""}></Image>
-        </div>
         <div className={"gameHeader"}>
           <div>❤️ 300/600</div>
           <div>💶 999.999</div>
@@ -30,16 +19,11 @@ export default function MainMenu({ user }: { user: AppUser | null }) {
         </div>
         <div className={"gameScreen"}>
           <div className={"scene"}>
-            <h2>Пустоши</h2>
-            <div className={"bgImage"}>
-              <Image src={bgImage} alt={""}></Image>
-            </div>
-            <div className={"footerImage"}>
-              <Image src={footerImage} alt={""}></Image>
-            </div>
+            <h2>Ты отправился в пустоши</h2>
             <div>
               <p style={{textAlign: "justify"}}>В борьбе за выживание ты бросил вызов беспощадной Пустоши.
                 Чем дальше ты заходишь в Пустоши - тем сложнее тебе будет там выжить. Путь займёт несколько минут.</p>
+              <h3 style={{textAlign: "center"}}>05:26</h3>
             </div>
           </div>
           <div className={"actions"}>
@@ -52,12 +36,17 @@ export default function MainMenu({ user }: { user: AppUser | null }) {
           </div>
         </div>
         <div className={"gameFooter"}>
-          <div>Персонаж</div>
-          <div>Инвентарь</div>
-          <div>Карта</div>
+          <div className={"button"}>Персонаж</div>
+          <div className={"button"}>Инвентарь</div>
+          <div className={"button"}>Карта</div>
+        </div>
+        <div className={"bgImage"}>
+          <Image src={bgImage} alt={""}></Image>
+        </div>
+        <div className={"footerImage"}>
+          <Image src={footerImage} alt={""}></Image>
         </div>
       </div>
-
     </>
   )
 }
