8d5d1918efb7ba15f6307b9dfad11b16b7067345
ralist <danilov@ralist.ru>
2024-02-22 15:12:28 +0400

    add back button to map

--- components/MapScreen.tsx
+++ components/MapScreen.tsx
@@ -1,8 +1,10 @@
 import {GameMap} from "@/lib/GameMap";
+import Link from "next/link";
 
 export default function MapScreen({map}: {map: GameMap}){
   return(
     <div className={"map"}>
+      <div><Link href={"/"}> Back </Link></div>
       {map.locations.map(location => {
         return <div key={location.id} className={"location"}>
           <div>{location.id} </div>
